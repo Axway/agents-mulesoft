de6ef6582942afdfd15298282aaf1e3960d7426a
Trevor Johnson <tjohnson@axway.com>
2021-05-10 19:11:52 -0700

    APIGOV-18627_tests fix test

--- pkg/cmd/discovery/root_test.go
+++ pkg/cmd/discovery/root_test.go
@@ -30,7 +30,7 @@ func Test_initSubscriptionManager(t *testing.T) {
 	mc := &anypoint.MockAnypointClient{}
 	cc := &mocks.MockCentralClient{}
 
-	sc := func(client anypoint.Client) subscription.SubscriptionPolicy {
+	sc := func(client anypoint.Client) subscription.Contract {
 		mh := &subscriptionMocks.MockContract{}
 		mh.On("Name").Return("sofake")
 		mh.On("Schema").Return("sofake schema")
