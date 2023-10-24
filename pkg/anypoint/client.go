c5a0e152a88b1879ca6086136c133abf83326ef0
Jason Collins <47123298+jcollins-axway@users.noreply.github.com>
2023-10-24 14:15:32 -0700

    update for client app oauth endpoint (#176)
    * update for client app oauth endpoint
    
    * cleanup

--- pkg/anypoint/client.go
+++ pkg/anypoint/client.go
@@ -153,7 +153,7 @@ func (c *AnypointClient) GetAccessToken() (string, *User, time.Duration, error) 
 		"password": c.password,
 	}
 	if c.clientID != "" {
-		url = c.baseURL + "/accounts/oauth2/token"
+		url = c.baseURL + "/accounts/api/v2/oauth2/token"
 		body = map[string]string{
 			"grant_type":    "client_credentials",
 			"client_id":     c.clientID,
